ce2ba9aed1d9af6c0f3cf8e96261770ce4c91a3f
“bd8” <bd8@sanger.ac.uk>
2024-01-22 11:48:53 +0000

    add args, runner code

--- kraken2ref/cmd.py
+++ kraken2ref/cmd.py
@@ -2,10 +2,11 @@ import argparse
 import os
 import shutil
 import logging
+from kraken2ref.src.kraken_taxonomy_report import KrakenTaxonomyReport
 
 
-# get the version number from a file that is created by setuptools_scm 
-# when the package is installed. 
+# get the version number from a file that is created by setuptools_scm
+# when the package is installed.
 try:
     from .version import version as __version__
     from .version import version_tuple
@@ -21,24 +22,39 @@ except ImportError:
 def args_parser():
     """
     Command line argument parser
-    """        
+    """
     parser = argparse.ArgumentParser(
-        description = "kraken2ref: extract reads to reference sequences from kraken2 outputs") 
+        description = "kraken2ref: extract reads to reference sequences from kraken2 outputs")
 
     parser.add_argument(
-        '-v', '--version', 
-        action='version', 
+        '-v', '--version',
+        action='version',
         version='kraken2ref ' + __version__ )
 
-    # TODO: add the parameters
-    
+    parser.add_argument(
+        '-i', '--in_file',
+        type = str,
+        required = True,
+        help = "The kraken2 taxonomy report (typically 'report.txt') to process. [str/pathlike]")
+
+    parser.add_argument(
+        '-t', '--min_read_threshold',
+        type = int,
+        required = False,
+        default = 5,
+        help = "The absolute minimum number of reads to use as threshold; taxa with fewer reads assigned to them will not be considered. [int][Default = 5]")
+
     return parser
-    
+
 def main():
-    
+
     args = args_parser().parse_args()
 
-    # TODO: run the command
+    my_tax_report = KrakenTaxonomyReport(in_file = args.in_file, min_abs_reads = args.min_read_threshold)
+    dump_dict = my_tax_report.pick_reference_taxid
+
+    for k, [t, p] in dump_dict.items():
+                print(f"Filename = {k}\ttaxa = {t}\tpath = {p}")
 
 if __name__ == "__main__":
     exit(main())

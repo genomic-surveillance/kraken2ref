ff2e0adee13ec56e8368302981529b2316b07085
“bd8” <bd8@sanger.ac.uk>
2024-05-08 09:57:47 +0100

    add subparser, sort_reads args, update fn main

--- kraken2ref/main.py
+++ kraken2ref/main.py
@@ -5,6 +5,7 @@ import argparse
 
 ## import driver module
 from kraken2ref.kraken2reference import KrakenProcessor
+from kraken2ref.sort_reads import sort_reads_by_tax
 
 ## collect version
 try:
@@ -31,19 +32,22 @@ def collect_args():
         required = True,
         help = "Sample ID. [str]")
 
-    parser.add_argument(
+    subparsers = parser.add_subparsers(title="subcommands", help="kraken2ref sub-commands", dest="run_mode")
+    report_parser = subparsers.add_parser("parse_report")
+
+    report_parser.add_argument(
         '-i', '--in_file',
         type = str,
         required = True,
         help = "The kraken2 taxonomy report (typically 'report.txt') to process. [str/pathlike]")
 
-    parser.add_argument(
+    report_parser.add_argument(
         '-o', '--outdir',
         type = str,
         required = True,
         help = "Full path to output directory. [str/pathlike]")
 
-    parser.add_argument(
+    report_parser.add_argument(
         '-x', '--suffix',
         type = str,
         required = False,
@@ -51,14 +55,14 @@ def collect_args():
         help = """Suffix to add to output JSON file.
                     Format will be: <sample_id>_<suffix>.json. [str]""")
 
-    parser.add_argument(
+    report_parser.add_argument(
         '-t', '--min_read_threshold',
         type = int,
         required = False,
         default = 100,
         help = "The absolute minimum number of reads to use as threshold; taxa with fewer reads assigned to them will not be considered. [int][Default = 100]")
 
-    parser.add_argument(
+    report_parser.add_argument(
         '-m', '--poll_method',
         type = str,
         required = False,
@@ -66,11 +70,68 @@ def collect_args():
         help = """Which polling method to use. [str] [Default = 'kmeans']
                     Valid choices: ['skew', 'kmeans', 'tiles']""")
 
-    parser.add_argument(
+    report_parser.add_argument(
         "-q", "--quiet",
         action = "store_true",
         required = False,
         help = "If specified, no stdderr will be run. [switch]")
+
+    sort_reads_parser = subparsers.add_parser("sort_reads")
+
+    sort_reads_parser.add_argument(
+            '-t', '--taxon_list',
+            required = False,
+            type = str,
+            help = "Comma-separated list of taxa to extract reads for. Eg: taxID_1,taxID_2... [str/pathlike]")
+
+    sort_reads_parser.add_argument(
+            "-k", "--kraken_out",
+            type = str,
+            required = True,
+            help = "Path to kraken2 output file. [str/pathlike]")
+
+    sort_reads_parser.add_argument(
+            "-fq1", "--fastq1",
+            type = str,
+            required = True,
+            help = "First FASTQ file of paired end reads. [str/pathlike]")
+
+    sort_reads_parser.add_argument(
+            "-fq2", "--fastq2",
+            type = str,
+            required = True,
+            help = "Second FASTQ file of paired end reads. [str/pathlike]")
+
+    sort_reads_parser.add_argument(
+            "-r", "--ref_json",
+            type = str,
+            required = False,
+            help = "Output JSON created by `kraken2ref parse_report`. [str/pathlike]")
+
+    sort_reads_parser.add_argument(
+            "-m", "--mode",
+            type = str,
+            required = False,
+            default="unique",
+            help = "Which mode to use while sorting reads ['unique', 'tree']")
+
+    sort_reads_parser.add_argument(
+            "-c", "--condense",
+            action = "store_true",
+            required = False,
+            help = "Whether to condense the outputs by root taxid. [switch]")
+
+    sort_reads_parser.add_argument(
+        "-u", "--update",
+        action = "store_true",
+        required = False,
+        help = "Whether to update the kraken2ref JSON inplace or create a new updated copy. [switch]")
+
+    sort_reads_parser.add_argument(
+        '-o', '--outdir',
+        type = str,
+        required = False,
+        help = "Full path to output directory. [str/pathlike]")
 
     args = parser.parse_args()
     return args
@@ -88,10 +149,16 @@ def main():
     logfile = os.path.join(fixed_outdir, f"{args.sample_id}_kraken2ref.log")
     logging.basicConfig(format='%(asctime)s | %(levelname)s | %(module)s - %(funcName)s | %(message)s', level=logging.NOTSET, datefmt="%Y-%m-%d %H:%M:%S", filename = logfile)
 
-    ## initialise driver module and analyse report
-    my_processor = KrakenProcessor(args.sample_id)
-    my_processor.analyse_report(input_kraken_report_file = args.in_file, input_threshold = args.min_read_threshold, input_method = args.poll_method, quiet=args.quiet)
-    my_processor.write_output(prefix = fixed_outdir, suffix = args.suffix)
+    if args.run_mode == "parse_report":
+
+        ## initialise driver module and analyse report
+        my_processor = KrakenProcessor(args.sample_id)
+        my_processor.analyse_report(input_kraken_report_file = args.in_file, input_threshold = args.min_read_threshold, input_method = args.poll_method, quiet=args.quiet)
+        my_processor.write_output(prefix = fixed_outdir, suffix = args.suffix)
+
+    if args.run_mode == "sort_reads":
+        sort_reads_by_tax(args)
+
 
 if __name__ == "main":
     main()

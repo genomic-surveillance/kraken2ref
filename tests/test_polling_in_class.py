2e9f52644f80284f21d933e14d1328a591d2a0ce
“bd8” <bd8@sanger.ac.uk>
2024-05-09 11:23:45 +0100

    remove old unused test

--- tests/test_polling_in_class.py
+++
@@ -1,5 +0,0 @@
-from kraken2ref.kraken2reference import KrakenProcessor
-
-my_processor = KrakenProcessor("test_k2r")
-my_processor.analyse_report(input_kraken_report_file="tests/test_set/with_fluseg_db/testset_50x.uniq.report.txt", input_threshold=100, input_method="kmeans")
-my_processor.write_output(suffix="decomposed")
